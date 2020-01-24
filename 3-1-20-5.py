207d202678d4e280be378717945e51934c173a2f
Mohanish Kashiwar <55909616+mk1107@users.noreply.github.com>
2020-01-24 15:34:58 +0530

    Add files via upload

---
+++ 3-1-20-5.py
@@ -0,0 +1,10 @@
+'''Define a function which can compute the sum of
+two numbers.'''
+
+def sum(a,b):
+    sum=a+b
+    print("SUM:- ",sum)
+
+a=int(input("ENTER No. 1:- "))
+b=int(input("ENTER No. 2:- "))
+sum(a,b)
