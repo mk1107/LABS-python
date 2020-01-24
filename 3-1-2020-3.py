016d1c022a73ee0f5a83219717bf95af376d8bf2
Mohanish Kashiwar <55909616+mk1107@users.noreply.github.com>
2020-01-24 15:04:39 +0530

    Add files via upload

---
+++ 3-1-2020-3.py
@@ -0,0 +1,11 @@
+''''Ask the user for a number. Depending on
+whether the number is even or odd, print out an
+appropriate message to the user.'''
+
+a=int(input('ENTER ANY No. '))
+
+if(a%2==0):
+    print("EVEN")
+
+else:
+    print("ODD")
