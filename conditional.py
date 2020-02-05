aac7fda4f44102cec48bef43a9319c954a8b75df
Mohanish Kashiwar <55909616+mk1107@users.noreply.github.com>
2020-02-05 08:46:11 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,30 @@
+kp = 2
+
+
+#if
+
+if(kp==2):
+    print("hello")
+
+
+
+#if-else
+
+
+if (0) :
+    print("hello")
+
+else:
+    print("false")
+
+#nested if-else
+
+
+if(1):
+    if(0):
+        print("hello")
+    elif (1):
+        print("MK11")
+else:
+    print("false")
+        
---
+++ loops.py
@@ -0,0 +1,26 @@
+k=0
+
+#While
+
+
+while(k<3):
+    print("Hello")
+    k += 1
+
+
+
+#for
+
+games = ["football","basketball","vollyball"]
+
+for i in games:
+    print(i)
+
+
+#nested loops
+
+while(k!=0):
+    k -= 1
+
+    for i in games:
+        print(i)
