2e29d2dc0f1b8140dcf7b92c03be1b775948d342
Mohanish Kashiwar <55909616+mk1107@users.noreply.github.com>
2020-02-06 08:35:03 +0530

    Add files via upload

---
+++ loop_control.py
@@ -0,0 +1,22 @@
+for l in 'Python':
+    if l=='h':
+        break
+    print("CURRENT LETTER:- ",l)
+
+print('\n')
+
+for l in 'Python':
+    if l=='h':
+        continue
+    print("CURRENT LETTER:- ",l)
+
+print('\n')
+
+for l in 'Python':
+    if l=='h':
+        pass
+        print('THIS IS PASS BLOCK')
+    print("CURRENT LETTER:- ",l)
+
+print('GOOD BYE!!!')
+
